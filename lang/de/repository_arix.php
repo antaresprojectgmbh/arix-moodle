0df0958f953889d1fc87460ab209a5c947711ed3
Rene Kaufmann <rkaufmann@chefkaufmann.lan>
2017-04-08 13:28:23 +0200

    fixed #1

--- lang/de/repository_arix.php
+++
@@ -1,9 +0,0 @@
-<?php
-$string['configplugin'] = 'Configuration for my plugin repository';
-$string['pluginname'] = 'arix';
-$string['arix_url'] = 'Arix URL';
-$string['kontext'] = 'Kontext';
-$string['token'] = 'Token';
-$string['username'] = "Benutzername";
-$string['password'] = "Passwort";
-$string['arix:view'] = 'arix';
--- lang/en/repository_arix.php
+++ lang/en/repository_arix.php
@@ -2,8 +2,6 @@
 $string['configplugin'] = 'Configuration for my plugin repository';
 $string['pluginname'] = 'arix';
 $string['arix_url'] = 'Arix URL';
-$string['kontext'] = 'Kontext';
+$string['kontext'] = 'Context';
 $string['token'] = 'Token';
-$string['username'] = "Username";
-$string['password'] = "Password";
 $string['arix:view'] = 'arix';
